45cf9039bebd5d6668ee54b01c8befffb9db4638
tranat97 <42818890+tranat97@users.noreply.github.com>
2019-12-04 04:08:04 -0500

    Add files via upload

---
+++ android/app/src/main/java/com/example/pottypoll/bathroomList.java
@@ -0,0 +1,42 @@
+package com.example.pottypoll;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class bathroomList extends AppCompatActivity {
+    ArrayList<BathroomStruct> brs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bathroom_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        restroom _restroom = new restroom(getApplicationContext());
+
+
+        RecyclerView brList = (RecyclerView) findViewById(R.id.brList);
+        brs = _restroom.getRestrooms(0,0);
+        brAdapter adapter = new brAdapter(brs);
+        brList.setAdapter(adapter);
+        brList.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    public void addBathroom(View view) {
+        Intent intent = new Intent(this, add_review_page.class);
+        startActivity(intent);
+    }
+
+}
---
+++ android/app/src/main/java/com/example/pottypoll/brAdapter.java
@@ -0,0 +1,77 @@
+package com.example.pottypoll;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class brAdapter extends
+        RecyclerView.Adapter<brAdapter.ViewHolder> {
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView nameTextView;
+        public Button reviewButton;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            nameTextView = (TextView) itemView.findViewById(R.id.bathroom_name);
+            reviewButton = (Button) itemView.findViewById(R.id.review_button);
+        }
+    }
+
+    // Store a member variable for the contacts
+    private List<BathroomStruct> bathrooms;
+
+    // Pass in the contact array into the constructor
+    public brAdapter(List<BathroomStruct> _bathrooms) {
+        bathrooms = _bathrooms;
+    }
+
+    @Override
+    public brAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.bathrooms, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(brAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        BathroomStruct bathroom = bathrooms.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = viewHolder.nameTextView;
+        textView.setText(bathroom.BUILDNAME+" floor "+bathroom.FLOOR+" ("+bathroom.GENDER+")");
+        Button button = viewHolder.reviewButton;
+        button.setText("Review");
+        button.setEnabled(true);
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return bathrooms.size();
+    }
+}
