f5060db2db71c50079430ef53ccebdc1c8235252
tranat97 <42818890+tranat97@users.noreply.github.com>
2019-12-04 03:33:40 -0500

    Add files via upload

--- android/app/src/main/java/com/example/pottypoll/add_review_page.java
+++ android/app/src/main/java/com/example/pottypoll/add_review_page.java
@@ -18,6 +18,7 @@ public class add_review_page extends AppCompatActivity {
     CheckBox shower;
     CheckBox sink;
     CheckBox papertowel;
+    int sh=0, s=0, pt=0;
 
 
     @Override
@@ -28,16 +29,27 @@ public class add_review_page extends AppCompatActivity {
 
     public void createBathroom(View view)
     {
-        //gender = (EditText)findViewById(R.id.gender);
-        //location = (EditText)findViewById(R.id.location);
-        //building = (EditText)findViewById(R.id.building);
-        //hours = (EditText)findViewById(R.id.hours);
-        //floor = (EditText)findViewById(R.id.floor);
-        //shower = (CheckBox)findViewById(R.id.shower);
-        //sink = (CheckBox)findViewById(R.id.sink);
-        //papertowel = (CheckBox)findViewById(R.id.papertowel);
-        //Toast message;
-        //message = Toast.makeText(getApplicationContext(), gender.getText().toString(), Toast.LENGTH_LONG);
-        //message.show();
+        gender = (EditText)findViewById(R.id.gender);
+        location = (EditText)findViewById(R.id.location);
+        building = (EditText)findViewById(R.id.building);
+        hours = (EditText)findViewById(R.id.hours);
+        floor = (EditText)findViewById(R.id.floor);
+        shower = (CheckBox)findViewById(R.id.shower);
+        sink = (CheckBox)findViewById(R.id.sink);
+        papertowel = (CheckBox)findViewById(R.id.papertowel);
+        if(shower.isChecked())
+            sh=1;
+        if(sink.isChecked())
+            s=1;
+        if(papertowel.isChecked())
+            pt=1;
+
+        restroom bathroom = new restroom(getApplicationContext());
+        //BathroomAdaptor brDB = new BathroomAdaptor(this);
+        if(bathroom.addRestroom(0, gender.getText().toString(), location.getText().toString(), 0, 0, building.getText().toString(), Integer.parseInt(floor.getText().toString()), hours.getText().toString(), sh, s, pt)) {
+            Toast message;
+            message = Toast.makeText(getApplicationContext(), "YAY", Toast.LENGTH_LONG);
+            message.show();
+        }
     }
 }
