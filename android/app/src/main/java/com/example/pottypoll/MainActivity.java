a3f523eb47282f01a61008f6bba8c673bb2e2ede
ezratl <etaimutyloomis@gmail.com>
2019-12-04 15:11:09 -0500

    Fixed account creation

--- android/app/src/main/java/com/example/pottypoll/MainActivity.java
+++ android/app/src/main/java/com/example/pottypoll/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -30,9 +31,34 @@ public class MainActivity extends AppCompatActivity {
 
         brDB = new BathroomAdaptor(this);
         userDB = new UserAdaptor(this);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("DATA", Context.MODE_PRIVATE);
+
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Toast message;
+
+                username = (EditText)findViewById(R.id.username);
+                password = (EditText)findViewById(R.id.password);
+                //message = Toast.makeText(getApplicationContext(), username.getText().toString(), Toast.LENGTH_LONG);
+                int userID = userDB.getID(username.getText().toString(), password.getText().toString());
+                if (userID < 0)
+                    Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_LONG).show();
+                else {
+                    sharedPreferences = getApplicationContext().getSharedPreferences("DATA", Context.MODE_PRIVATE);
+                    sharedPreferences.edit().putInt("USERID", userID).apply();
+                    //message.show();
+
+                    Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
-    public void login(View view){
+    /*public void login(View view){
         Toast message;
 
         username = (EditText)findViewById(R.id.username);
@@ -47,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    }
+    }*/
 
     public void createUser (View view)
     {
@@ -57,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         message = Toast.makeText(getApplicationContext(), username.getText().toString(), Toast.LENGTH_LONG);
         int userID = (int)userDB.insertData(username.getText().toString(), password.getText().toString(), "name@email.com", 0);
         if (userID > 0)
-            message = Toast.makeText(getApplicationContext(), "YAY" , Toast.LENGTH_LONG);
+            message = Toast.makeText(getApplicationContext(), "Account created successfully" , Toast.LENGTH_LONG);
         sharedPreferences.edit().putInt("USERID",userID).apply();
         message.show();
     }
